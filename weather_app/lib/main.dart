cb28ba5632bed3c1fadb727c860c863e6b2ff32d
temilolasayo <alajikitemilola@gmail.com>
2023-09-01 11:49:47 +0100

    Added the splash screen

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/view/home/home.dart';
 import 'package:weather_app/view_model/home_viewmodel.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MainApp());
+  // FlutterNativeSplash.remove();
 }
 
 class MainApp extends StatelessWidget {
