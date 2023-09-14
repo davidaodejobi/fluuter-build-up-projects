a35b561325698b5dfe043c4af705562790b26559
temilolasayo <alajikitemilola@gmail.com>
2023-09-14 13:37:37 +0100

    Added validation check for the textfield on the Add Location page

--- weather_app/lib/view/location/widgets/add_location.dart
+++ weather_app/lib/view/location/widgets/add_location.dart
@@ -3,7 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:weather_app/constants/app_colors.dart';
 import 'package:weather_app/models/location_data.dart';
 
-late String newCity;
+final _controller = TextEditingController();
+final _formKey = GlobalKey<FormState>();
 
 class AddLocation extends StatelessWidget {
   const AddLocation({super.key});
@@ -52,45 +53,66 @@ class AddLocation extends StatelessWidget {
                 ),
                 //TODO: 4 Add validation check for this field so that it can't be empty
                 //TODO: 5 CHange the blue line showing under the text field to black (and white)
-                TextFormField(
-                  onChanged: (value) {
-                    newCity = value;
-                  },
-                  cursorColor: AppColors.kBackgroundColor,
-                  autofocus: true,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall
-                      ?.copyWith(color: Colors.black),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    locationData.addNewCity(newCity, -10, "Light Drizzle");
-
-                    Navigator.pop(context);
-                  },
-                  style: ButtonStyle(
-                    fixedSize: MaterialStatePropertyAll<Size>(
-                      Size(
-                        MediaQuery.of(context).size.height * 1,
-                        50,
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _controller,
+                        // onChanged: (value) {
+                        //   newCity = value;
+                        // },
+                        validator: (value) {
+                          if (value == null || value == "") {
+                            return "Enter a valid city name";
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                        cursorColor: AppColors.kBackgroundColor,
+                        autofocus: true,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall
+                            ?.copyWith(color: Colors.black),
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                    backgroundColor: const MaterialStatePropertyAll(
-                      AppColors.kBackgroundColor,
-                    ),
-                  ),
-                  child: Text(
-                    "Add",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium
-                        ?.copyWith(color: Colors.white),
-                    // style: TextStyle(color: kSecondaryColor),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            locationData.addNewCity(
+                                _controller.text, -10, "Light Drizzle");
+                            Navigator.pop(context);
+                          }
+                        },
+                        style: ButtonStyle(
+                          fixedSize: MaterialStatePropertyAll<Size>(
+                            Size(
+                              MediaQuery.of(context).size.height * 1,
+                              50,
+                            ),
+                          ),
+                          backgroundColor: const MaterialStatePropertyAll(
+                            AppColors.kBackgroundColor,
+                          ),
+                        ),
+                        child: Text(
+                          "Add",
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(color: Colors.white),
+                          // style: TextStyle(color: kSecondaryColor),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
